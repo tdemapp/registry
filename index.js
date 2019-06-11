8ebcb0b5f8bb408890c077c1fbd651d24c5ab846
N U R O ™ <bendixon50@gmail.com>
2019-06-11 12:39:55 +0100

    :boom: Complete restructure (Including caching)

--- index.js
+++ index.js
@@ -1,30 +1,99 @@
-const fetch = require('node-fetch');
-const cache = require('micro-cacheable');
+'use strict';
 
-module.exports = cache(24 * 60 * 60 * 1000, async (req, res) => {
+const fetch = require('node-fetch');
+const NodeCache = require('node-cache');
+
+const download = (url) => {
+	return new Promise((resolve, reject) => {
+		// API fetch header options
+		const fetchOptions = {
+			method: 'GET',
+			headers: {
+				Authorization: `token ${process.env.GH_ACCESS_TOKEN}`,
+			},
+		};
+
+		// Memory cache for storing extensions
+		const extensionCache = new NodeCache({
+			checkperiod: 3600, // Cache for 1 hour
+		});
+
+		// Fetch from GitHub API
+		fetch(url, fetchOptions)
+			.then(async (res) => {
+				if (res.status < 200 || res.status >= 300) reject(res.status);
+
+				const extension = await res.json();
+				let extensionName;
+
+				if (Array.isArray(extension)) {
+					extensionName = extension[0].name.replace(/\.[^/.]+$/, '');
+				} else {
+					extensionName = extension.name.replace(/\.[^/.]+$/, '');
+				}
+
+				// Cache the extension if it doesn't already exist
+				try {
+					extensionCache.get(extensionName, (err, value) => {
+						if (err) console.error('Error getting cache: ', err);
+
+						if (value === undefined) {
+							extensionCache.set(extensionName, extension, (err, success) => {
+								if (err || !success) console.error('Error setting cache: ', err);
+							});
+						}
+					});
+				} catch (err) {
+					console.error('Cache Error: ', err);
+				}
+
+				resolve(extension);
+			})
+			.catch((err) => {
+				reject(err);
+			});
+	});
+};
+
+module.exports = async (req, res) => {
+	// Response object tempkate structure
+	const result = (success, message) =>
+		res.end(
+			JSON.stringify(
+				{
+					success,
+					message,
+				},
+				null,
+				2
+			)
+		);
+
+	// Set response header content type
 	res.setHeader('Content-Type', 'application/json');
 
-	const fetchOptions = {
-		method: 'GET',
-		headers: {
-			'Authorization': `token ${process.env.GH_ACCESS_TOKEN}`,
-		},
-	};
+	// GitHub API prefix URL
+	const apiPrefix = 'https://api.github.com/repos/tdemapp/registry/contents/extensions';
 
-	try {
-		const response = await fetch(
-			`https://api.github.com/repos/tdemapp/registry/contents/extensions${req.url}`,
-			fetchOptions
-		);
-		let data;
-
-		if (response.status >= 200) {
-			data = await response.json();
-		}
-
-		res.end(JSON.stringify({ success: true, message: data }, null, 2));
-	} catch (err) {
-		console.error(err);
-		res.end(JSON.stringify({ success: false, message: err.message }, null, 2));
+	// If no extension name provided (Request URL), fetch all and respond
+	if (req.url === '/') {
+		await download(apiPrefix)
+			.then((data) => {
+				result(true, data);
+			})
+			.catch((err) => {
+				result(false, JSON.stringify(err));
+			});
 	}
-});
+
+	// If extension name provided, fetch it and respond with it
+	await download(
+		`https://api.github.com/repos/tdemapp/registry/contents/extensions${req.url}.json`
+	)
+		.then((data) => {
+			result(true, data);
+		})
+		.catch((err) => {
+			result(false, JSON.stringify(err));
+		});
+};
